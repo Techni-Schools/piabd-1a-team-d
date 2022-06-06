dab6cb6cd5798c2a64c13cfb8e2c681970c8ac02
ErnestNoga <ernest@ernnog.com>
2022-06-06 15:11:29 +0200

    Added check if username doesn't contain illegal chars and if any of the fields are empty

--- src/resources/account/register/.post.php
+++ src/resources/account/register/.post.php
@@ -1,6 +1,15 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT']  . '/resources/.mysqli.php';
 
+if (preg_match("/[^-\w]/", $_POST['username'])) {
+    header('Refresh: 3');
+    die('<h1>This username contains illegal chars</h1>');
+}
+if (!$_POST['username'] || !$_POST['email'] || !$_POST['password']) {
+    header('Refresh: 3');
+    die('<h1>None of the fields can be empty</h1>');
+}
+
 $username = "'" . bin2hex($_POST['username']) . "'";
 $email = "'" . bin2hex($_POST['email']) . "'";
 $password = "'" . bin2hex(password_hash($_POST['password'], PASSWORD_DEFAULT)) . "'";
