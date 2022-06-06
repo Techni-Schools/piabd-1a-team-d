f5312b001605b0cdfad2b7d720daaa9742cd910e
ErnestNoga <ernest@ernnog.com>
2022-06-06 15:18:44 +0200

    Deleted list.php POST method access

--- src/note/list.php
+++ src/note/list.php
@@ -6,7 +6,5 @@ if (!isset($_SESSION['username'])) {
 }
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
     require $_SERVER['DOCUMENT_ROOT'] . '/resources/note/list/.get.php';
-elseif ($_SERVER['REQUEST_METHOD'] == 'POST')
-    require $_SERVER['DOCUMENT_ROOT'] . '/resources/note/list/.post.php';
 else
     include $_SERVER['DOCUMENT_ROOT'] . '/resources/http_codes/405.php';
