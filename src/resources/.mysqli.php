ba7d3259370ba0c56b4c6ad38543bd83804f35b8
ErnestNoga <ernest@ernnog.com>
2022-06-07 10:04:06 +0200

    Changed used database from auth to sqnote

--- src/resources/.mysqli.php
+++ src/resources/.mysqli.php
@@ -1,4 +1,4 @@
 <?php
 $conn = new mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASS']);
-mysqli_select_db($conn, 'auth');
+mysqli_select_db($conn, 'sqnote');
 $conn->autocommit(false);
